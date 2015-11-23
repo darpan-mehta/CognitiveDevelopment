bc443f061089eb3cd827cc5107641a2052b00849
schoeberle <sarah.schoeberle@drake.edu>
2015-11-23 10:45:42 -0600

    Added DrawView Class

---
+++ app/src/main/java/com/cogdev/cognitivedevelopment/DrawView.java
@@ -0,0 +1,68 @@
+package com.cogdev.cognitivedevelopment;
+
+/**
+ * Created by Sarah on 11/23/2015.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class DrawView extends View implements OnTouchListener {
+    private static final String TAG = "DrawView";
+
+    List<Point> points = new ArrayList<Point>();
+    Paint paint = new Paint();
+
+    public DrawView(Context context) {
+        super(context);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setOnTouchListener(this);
+
+        paint.setColor(Color.BLUE);
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.square);
+        canvas.drawBitmap(b,50,200,null);
+        for (Point point : points) {
+            canvas.drawCircle(point.x, point.y, 7, paint);
+            // Log.d(TAG, "Painting: "+point);
+        }
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        // if(event.getAction() != MotionEvent.ACTION_DOWN)
+        // return super.onTouchEvent(event);
+        Point point = new Point();
+        point.x = event.getX();
+        point.y = event.getY();
+        points.add(point);
+        invalidate();
+        Log.d(TAG, "point: " + point);
+        return true;
+    }
+}
+
+class Point {
+    float x, y;
+
+    @Override
+    public String toString() {
+        return x + ", " + y;
+    }
+}
+
